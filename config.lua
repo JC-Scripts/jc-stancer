007481e372a29267d5e6dc4ab6a8b862aca5daf4
ArChrisVa <arvachri@csd.auth.gr>
2024-09-16 21:28:11 +0300

    Added a config to better handle which cars should be stanced

---
+++ config.lua
@@ -0,0 +1,18 @@
+
+-- Insert car SPAWN NAMES you want to stance inside this config
+-- Make sure every car you insert here has these values fCamberFront, fCamberRear under <SubHandlingData>
+-- <SubHandlingData>
+--        <Item type="CCarHandlingData">
+--          <fCamberFront value="-0.000000"/>
+--          <fCamberRear value="-0.000000"/>
+-- </SubHandlingData>
+
+
+config = {
+    ['stancableCars'] = 
+    {
+        'ARGENTO',
+        'ELEGYRH7',
+    },
+    ['keybind'] = 'k'
+}
